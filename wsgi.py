8e51b32b5f8b840dc14d97b89094e1176eba03dc
XIAORUI YU <yuxiaorui@users.noreply.github.com>
2015-06-01 17:24:40 +0800

    Update wsgi.py
    安装和初始化 LeanEngine 中间件

--- wsgi.py
+++ wsgi.py
@@ -20,7 +20,8 @@ PORT = int(os.environ['LC_APP_PORT'])
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "avoscloud_django.settings")
 
 leancloud.init(APP_ID, master_key=MASTER_KEY)
-application = get_wsgi_application()
+app = get_wsgi_application()
+engine = leancloud.Engine(app)
 
 if __name__ == '__main__':
     # 只在本地开发环境执行的代码
