7a5ad5bf89b3935bf68cbb62e4561c8a6b3e588b
yuxiaorui <yu@oddsfair.cn>
2015-06-02 22:53:59 +0800

    chang debug

--- avoscloud_django/settings.py
+++ avoscloud_django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uu47(vufggjjt8*34=&8)js_n_85q4zc+^f*)!l)4*@5yb%=ot'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
